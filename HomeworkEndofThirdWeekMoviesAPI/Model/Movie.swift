331cf13c8d97ae4e05acc696eaf5ebb9075472f2
Forrest Filler <forrestfiller@users.noreply.github.com>
2016-06-25 10:58:53 -0400

    added model 'Movie', folder 'Model', folder for 'Images'-no images yet, and created the Model vars based upon json file names

---
+++ HomeworkEndofThirdWeekMoviesAPI/Model/Movie.swift
@@ -0,0 +1,19 @@
+//
+//  Movie.swift
+//  HomeworkEndofThirdWeekMoviesAPI
+//
+//  Created by Forrest Filler on 6/25/16.
+//  Copyright © 2016 ffdevappsff. All rights reserved.
+//
+
+import UIKit
+
+class Movie: NSObject {
+    
+    var id: String! //id number from the json
+    var title: String! //title of movie
+    var image: String! //movie image
+    var vote_average: String! //average of user votes
+    
+
+}
