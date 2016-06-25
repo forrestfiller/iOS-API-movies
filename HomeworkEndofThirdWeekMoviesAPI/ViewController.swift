a15e0fa0bde76b4e5d993637d1ecf16ad6f9423e
Forrest Filler <forrestfiller@users.noreply.github.com>
2016-06-25 10:44:12 -0400

    first commit

---
+++ HomeworkEndofThirdWeekMoviesAPI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HomeworkEndofThirdWeekMoviesAPI
+//
+//  Created by Forrest Filler on 6/25/16.
+//  Copyright © 2016 ffdevappsff. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ README.md
@@ -0,0 +1,7 @@
+# README
+
+**Homework App**
+  - Use a tableView
+  - Utilize the api address that Dan shared
+  - Anytype of App is fine
+
